b7be2abda3788b32c93e5f43aa0183cbb36a773c
Fabian Fuentes <fabianfuentes1804643@hotmail.com>
2019-08-14 15:22:21 -0500

    Initial commit

---
+++ Proyecto/Poject.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectosSenova
+{
+    public class Proyecto
+    {
+        #region Atributos 
+        public List<Proyecto> proyectos = new List<Proyecto>();
+        #endregion
+
+        #region Propiedades
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+
+        #endregion
+
+        #region Constructores
+        public Proyecto(string nombre, double codigo, string area, int duracion)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+        }
+        public Proyecto()
+        {
+
+        }
+
+        public Proyecto(string area, int duracion)
+        {
+            Area = area;
+            Duracion = duracion;
+        }
+        #endregion
+
+        #region Metodos
+        public void ObtenerMayores()
+        {
+            List<Proyecto> pro20 = new List<Proyecto>();
+            foreach (var item in proyectos)
+
+            {
+                if (item.Codigo>20)
+                {
+                    pro20.Add(item);
+                }
+            }
+            ImprimirProyecto(pro20);
+        }
+
+        public void IngresarProyectos()
+        {
+
+            var res = "y";
+            while (res=="y")
+            {
+
+            Proyecto miproyecto = new Proyecto();
+            Console.WriteLine("Ingrese el Nombre del proyecto");
+            miproyecto.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el Codigo del proyecto");
+            miproyecto.Codigo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Area del proyecto");
+            miproyecto.Area = Console.ReadLine();
+            Console.WriteLine("Ingrese la Duracion del proyecto");
+            miproyecto.Duracion = int.Parse(Console.ReadLine());
+            proyectos.Add(miproyecto);
+            Console.WriteLine("Desea agregar un nuevo proyecto");
+            res=(Console.ReadLine());
+            }
+
+        }
+        public void ImprimirProyecto(List<Proyecto>proyecto1)
+        {
+            foreach (var item in proyecto1)
+            {
+            Console.WriteLine($"Nombre: {item.Nombre}");
+            Console.WriteLine($"Codigo: {item.Codigo}");
+            Console.WriteLine($"Duracion: {item.Duracion}");
+            Console.WriteLine($"Area: {item.Area}");
+            }
+        }
+        #endregion
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Proyecto/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ProyectosSenova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Proyecto miProyecto = new Proyecto();
+            //Proyecto miProyecto2 = new Proyecto();
+            Proyecto miProyecto3 = new Proyecto();
+            //Proyecto miProyecto4 = new Proyecto("Realidad aumentada", 12540, "Sistemas", 10);
+            //Proyecto miProyecto5 = new Proyecto("Talento Humano", 5);
+
+
+            //miProyecto5.ImprimirProyecto();
+            //miProyecto4.ImprimirProyecto();
+            miProyecto3.IngresarProyectos();
+            miProyecto3.ImprimirProyecto(miProyecto3.proyectos);
+
+
+            //miProyecto.Area = "Sistemas";
+            //miProyecto.Codigo = 12000;
+            //miProyecto.Duracion = 13;
+            //miProyecto.Nombre = "Desarrollo internet de las cosas IOT";
+            //miProyecto2.Area = "Redes";
+            //miProyecto2.Codigo = 13000;
+            //miProyecto2.Duracion = 14;
+            //miProyecto2.Nombre = "Desarrollo arduino";
+            //Console.WriteLine($"Area: {miProyecto.Area}");
+            //Console.WriteLine($"Codigo: {miProyecto.Codigo}");
+            //Console.WriteLine($"Duracion: {miProyecto.Duracion}");
+            //Console.WriteLine($"Nombre: {miProyecto.Nombre}");
+            //Console.WriteLine($"Area: {miProyecto2.Area}");
+            //Console.WriteLine($"Codigo: {miProyecto2.Codigo}");
+            //Console.WriteLine($"Duracion: {miProyecto2.Duracion}");
+            //Console.WriteLine($"Nombre: {miProyecto2.Nombre}");
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
